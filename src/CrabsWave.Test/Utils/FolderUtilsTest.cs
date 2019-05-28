02dbc19969c1c6fc2f2560b19a0c559dc7373a9e
Alexandre Borges <alexandre_borges@dell.com>
2019-05-28 06:33:32 -0300

    Removing tests that don't works well

--- src/CrabsWave.Test/Utils/FolderUtilsTest.cs
+++ src/CrabsWave.Test/Utils/FolderUtilsTest.cs
@@ -1,5 +1,4 @@
 ﻿using System.IO;
-using System.Runtime.InteropServices;
 using CrabsWave.Utils.IO;
 using FluentAssertions;
 using Xunit;
@@ -15,37 +14,5 @@ namespace CrabsWave.Test.Utils
             Directory.Exists(sut).Should().BeTrue();
         }
 
-        [Theory]
-        [InlineData("testFile.exe", "C:\\MyOnPocs\\CrawlerWave\\src\\CrabsWave.Test\\bin\\Debug\\netcoreapp2.2", true)]
-        [InlineData("testFile", "C:\\MyOnPocs\\CrawlerWave\\src\\CrabsWave.Test\\bin\\Debug\\netcoreapp2.2", true)]
-        [InlineData("", "", false)]
-        [InlineData("testFile.exe", "", false)]
-        [InlineData("", "C:\\MyOnPocs\\CrawlerWave\\src\\CrabsWave.Test\\bin\\Debug\\netcoreapp2.2", false)]
-        public void ShouldCheckIfFileExist(string fileName, string directory, bool exist)
-        {
-            var useNewFile = !string.IsNullOrWhiteSpace(fileName) && !string.IsNullOrWhiteSpace(directory);
-
-            if (useNewFile)
-            {
-                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) && !fileName.Contains(".exe"))
-                    fileName += ".exe";
-
-                CreateFakeFile($"{directory}{Path.DirectorySeparatorChar}{fileName}");
-            }
-
-            var sut = FolderUtils.SafeCheckFileExist(fileName, directory);
-
-            if (exist)
-                sut.Should().BeTrue();
-            else
-                sut.Should().BeFalse();
-
-            if (useNewFile)
-                DeleteFakeFile($"{directory}{Path.DirectorySeparatorChar}{fileName}");
-        }
-
-        private void CreateFakeFile(string filePath) => File.AppendAllText(filePath, "someText");
-
-        private void DeleteFakeFile(string filePath) => File.Delete(filePath);
     }
 }
