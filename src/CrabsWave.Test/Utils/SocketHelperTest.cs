5dcf5c28c4640b1fe630307634335b8bc685ef1a
Alexandre Borges <alexandre_borges@dell.com>
2019-05-30 06:26:14 -0300

    Adding tests for socket ports

---
+++ src/CrabsWave.Test/Utils/SocketHelperTest.cs
@@ -0,0 +1,32 @@
+﻿using CrabsWave.Utils.IO;
+using FluentAssertions;
+using Xunit;
+
+namespace CrabsWave.Test.Utils
+{
+    public class SocketHelperTest
+    {
+        [Fact]
+        public void ShouldGetIntPortWithFourNumbers()
+        {
+            var port = SocketHelper.GetRandomPortToTry();
+            port.Should().BeInRange(1000, 9999);
+        }
+
+        [Fact]
+        public void ShouldHavePortsInUse()
+        {
+            var portsInUse = SocketHelper.GetSocketPortsInUse();
+            portsInUse.Should().NotBeEmpty();
+        }
+
+        [Fact]
+        public void ShouldGetNewPortToUse()
+        {
+            var portsInUse = SocketHelper.GetSocketPortsInUse();
+            var newPort = SocketHelper.GetNewSocketPort();
+            newPort.Should().BeInRange(1000, 9999);
+            portsInUse.Should().NotContain(newPort);
+        }
+    }
+}
