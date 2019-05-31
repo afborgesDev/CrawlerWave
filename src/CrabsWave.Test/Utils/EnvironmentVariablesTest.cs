39b14661c638c6938bcb3d9abe4cd0c52ea741d1
Alexandre Borges <alexandre_borges@dell.com>
2019-05-31 06:10:43 -0300

    Adding tests for environment utils

---
+++ src/CrabsWave.Test/Utils/EnvironmentVariablesTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using CrabsWave.Utils.IO;
+using FluentAssertions;
+using Xunit;
+
+namespace CrabsWave.Test.Utils
+{
+    public class EnvironmentVariablesTest
+    {
+        [Fact]
+        public void ShouldSetVariable()
+        {
+            const string VariableName = "MyOnVariable";
+            const string VariableValue = "MyOnVariableValue";
+
+            EnvironmentVariablesUtil.SetVariableValue(VariableName, VariableValue);
+
+            var value = Environment.GetEnvironmentVariable(VariableName);
+
+            value.Should().NotBeNullOrEmpty();
+            value.Should().BeEquivalentTo(VariableValue);
+        }
+    }
+}
