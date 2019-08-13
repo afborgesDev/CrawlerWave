fc970e6bafdfb640856632ce9251b7316356e02f
Alexandre Borges <alexandre_borges@dell.com>
2019-08-13 06:14:44 -0300

    Adding package configurations

--- src/CrawlerWave.Utils/IO/SocketHelper.cs
+++ src/CrawlerWave.Utils/IO/SocketHelper.cs
@@ -24,12 +24,14 @@ namespace CrawlerWave.Utils.IO
 
         public static int GetRandomPort()
         {
-            var provider = new RNGCryptoServiceProvider();
-            var byteArrayNumber = new byte[5];
-            provider.GetBytes(byteArrayNumber);
-            var number = BitConverter.ToInt32(byteArrayNumber, 0);
-            if (number < 0) number *= -1;
-            return int.Parse(number.ToString().Substring(0, 5));
+            using (var provider = new RNGCryptoServiceProvider())
+            {
+                var byteArrayNumber = new byte[5];
+                provider.GetBytes(byteArrayNumber);
+                var number = BitConverter.ToInt32(byteArrayNumber, 0);
+                if (number < 0) number *= -1;
+                return int.Parse(number.ToString().Substring(0, 5));
+            }
         }
     }
 }
